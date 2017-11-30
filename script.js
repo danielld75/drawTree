f0ff998072b0d39df7058ab56a2aa3ef8f73a8d1
danielld75 <danielld@poczta.fm>
2017-11-30 16:58:12 +0100

    Reduce variable in code

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 var drawTree = function(h) {
   for (var i = 1; i <= h; i++) {
     var star = "";
-    var space = "";
     for (var j = i; j < h; j++) {
-      space += " ";
+      star += " ";
     }
     for (var j = 1; j <= (2 * i - 1); j++) {
       star += "*";
     }
-    console.log(space + star);
+    console.log(star);
   }
 };
 
