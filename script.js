7c1cad36806a58a55eafeda0ee46b5b304ebc40a
danielld75 <danielld@poczta.fm>
2017-11-30 15:59:31 +0100

    Add drawTree function

---
+++ script.js
@@ -0,0 +1,17 @@
+var drawTree = function(h) {
+  for (var i = 1; i <= h; i++) {
+    var star = "";
+    var space = "";
+    for (var j = i; j < h; j++) {
+      space += " ";
+    }
+    for (j = 1; j <= (2 * i - 1); j++) {
+      star += "*";
+    }
+    console.log(space + star);
+  }
+};
+
+drawTree(12);
+drawTree(9);
+drawTree(13);
